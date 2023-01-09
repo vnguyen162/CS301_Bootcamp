459d29932cb23ef9ba1d5b0412b2def746b4416f
vnguyen162 <tuongving4@gmail.com>
2023-01-09 15:01:24 -0800

    Added function example

---
+++ Day 1/Functions/Function_practice.cpp
@@ -0,0 +1,7 @@
+/*
+ * Write a Swap function and test it in main function
+ */
+
+void swapValues(int& variable1, int& variable2);
+//  Precondition: variable1 and variable 2 have been given values
+//  Postcondition: the values of variable1 and variable 2 are interchanged
