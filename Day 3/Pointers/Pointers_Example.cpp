76fe88dffbc358a090dfdcf481d99f70878eb120
vnguyen162 <tuongving4@gmail.com>
2023-01-11 03:27:49 -0800

    Added pointer example

---
+++ Day 3/Pointers/Pointers_Example.cpp
@@ -0,0 +1,33 @@
+//
+// Created by vnguy on 1/10/2023.
+//
+#include <iostream>
+using namespace std;
+int* Create_Array(const int size);
+int* Set_Array(int* arr, const int size);
+
+int main()
+{
+    int size =10;
+    int*myArray = Create_Array(size);
+    int*newArray = Set_Array(myArray, size);
+    for (int i =0 ; i<size; i++)
+    {
+        cout<< newArray[i]<<endl;
+    }
+    return 0;
+}
+
+int* Create_Array(const int size)
+{
+    int arr[size];
+    return arr;
+}
+int* Set_Array(int* arr, int size)
+{
+    for (int i = 0; i<size;i++)
+    {
+        arr[i]=i;
+    }
+    return arr;
+}
