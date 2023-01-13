b48d72c10d1e5b14ec85455dd68db10d6a30832a
vnguyen162 <tuongving4@gmail.com>
2023-01-13 12:56:52 -0800

    Added Functions_practice.cpp

--- Day 1/Functions/Functions_practice.cpp
+++ Day 1/Functions/Functions_practice.cpp
@@ -1,7 +1,22 @@
 /*
  * Write a Swap function and test it in main function
  */
+#include<iostream>
+using namespace std;
 
 void swapValues(int& variable1, int& variable2);
 //  Precondition: variable1 and variable 2 have been given values
 //  Postcondition: the values of variable1 and variable 2 are interchanged
+
+int main()
+{
+
+
+    return 0;
+}
+
+void swapValues(int& variable1, int& variable2)
+{
+
+}
+
