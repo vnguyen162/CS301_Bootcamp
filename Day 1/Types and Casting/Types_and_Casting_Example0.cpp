c82ab1d2df18643ecfec72743fe00858a21a652b
vnguyen162 <tuongving4@gmail.com>
2023-01-13 12:55:01 -0800

    Added Types_and_Casting_Example0.cpp

---
+++ Day 1/Types and Casting/Types_and_Casting_Example0.cpp
@@ -0,0 +1,22 @@
+//
+// Created by vnguy on 1/8/2023.
+//
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int rise;
+    int run;
+
+    //    cout << "Rise: ";
+    //    cin >> rise;
+    //    cout << " Run: ";
+    //    cin >> run;
+
+    rise = 3;
+    run = 2;
+    double slope = static_cast<double>(rise)/run;
+    cout << "Slope = " << slope << endl;
+
+}
